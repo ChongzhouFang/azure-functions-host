940a4e1e8d34b6f0ae892d20e1e078233fb9f23d
ChongzhouFang <chongzhoufang@outlook.com>
2023-12-27 16:27:36 -0800

    initial upload of jsondumpsloads

---
+++ src/WebJobs.Script.WebHost/Resources/Functions/Jsondumpsloads/__init__.py
@@ -0,0 +1,19 @@
+import azure.functions as func
+from urllib.request import urlopen
+from time import time
+import json
+
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    link = 'https://raw.githubusercontent.com/prust/wikipedia-movie-data/master/movies.json'
+    start = time()
+    f = urlopen(link)
+    data = f.read().decode("utf-8")
+    network = time() - start
+
+    start = time()
+    json_data = json.loads(data)
+    str_json = json.dumps(json_data, indent=4)
+    latency = time() - start
+
+    return func.HttpResponse("latency : " + str(latency) + "/ network : " + str(network))
