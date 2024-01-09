2d3fe88f55e91c1a10f3b68eeb4ceed44c222a22
ChongzhouFang <chongzhoufang@outlook.com>
2024-01-09 13:32:09 -0800

    update return message

--- src/WebJobs.Script.WebHost/Resources/Functions/Startup/__init__.py
+++ src/WebJobs.Script.WebHost/Resources/Functions/Startup/__init__.py
@@ -8,4 +8,4 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
 
     latency = str(startTime)
 
-    return func.HttpResponse('benchmark startup runtime: ' + latency)
+    return func.HttpResponse('benchmark startup runtime: ' + latency + '\n')
